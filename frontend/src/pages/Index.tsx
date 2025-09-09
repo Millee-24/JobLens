a4d7e3c23217c870e89dec58434799e2ad020721
Millee-24 <milimittal29@gmail.com>
2025-09-09 23:15:10 +0530

    updated

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -42,7 +42,7 @@ const Index = () => {
       formData.append('job_description', jobDescFile);
 
       // Replace with your actual backend URL
-      const response = await fetch('https://8499ffc5d5c9.ngrok-free.app/analyze', {
+      const response = await fetch('https://e7434454d181.ngrok-free.app/analyze', {
         method: 'POST',
         body: formData,
       });
